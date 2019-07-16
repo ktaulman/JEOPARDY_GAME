c7bfcd7af693dd0b6718679d3065f8ff393af0f9
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-15 17:27:28 -0700

    Updates

---
+++ precache-manifest.318585fe6de63454a4c442c713c4d3bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b020ab63df742bcdd7df2cfa3fa12d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aabbee89e49bc58080dd",
+    "url": "/static/css/main.81e26fda.chunk.css"
+  },
+  {
+    "revision": "b8635b1bb0ce638d5b54",
+    "url": "/static/js/2.137b9f13.chunk.js"
+  },
+  {
+    "revision": "aabbee89e49bc58080dd",
+    "url": "/static/js/main.831b9e48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "254e6b1b1ba1155c9843ea98df78472d",
+    "url": "/static/media/Alex_Avatar.254e6b1b.png"
+  },
+  {
+    "revision": "4ebe406d0db16d8ff1350abd88ef812c",
+    "url": "/static/media/Alex_Name.4ebe406d.png"
+  },
+  {
+    "revision": "1042b7916ecc1956c1dd00a3350f24b4",
+    "url": "/static/media/Stage.1042b791.png"
+  },
+  {
+    "revision": "061441c5ca8aa132318c281882b1421d",
+    "url": "/static/media/man.061441c5.png"
+  },
+  {
+    "revision": "92ff2427616c52468277da21869e2e38",
+    "url": "/static/media/robot.92ff2427.png"
+  },
+  {
+    "revision": "b68cb98a33e2663f50993de48352e277",
+    "url": "/static/media/woman.b68cb98a.png"
+  }
+]);
