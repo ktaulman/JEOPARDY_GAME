6313252f3a3aedfd608f1818ebbb11425f1cc018
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-17 16:21:11 -0700

    added game questions

---
+++ src/main_game/gameQuestions.js
@@ -0,0 +1,73 @@
+const gameQuestions=[
+    {
+        "category":"Athletes",
+        "questions":[
+            {
+                "points":"100",
+                "question":"This Chicago Bull wore #23 in 90s.",
+                "format":"Who is:",
+                "answer":"Michael Jordan"   
+            },
+            {
+                "points":"200",
+                "question":"She has a sister name Venus who plays the same sport.",
+                "format":"Who is:",
+                "answer":"Serena Williams"  
+            },
+            {
+                "points":"300",
+                "question":"This two-sport athlete played for both the LA Raiders and KC Royals",
+                "format":"Who is:",
+                "answer":"Bo Jackson"  
+            }
+        ]
+    },
+    {
+        "category":"Movies",
+        "questions":[
+            {
+                "points":"100",
+                "question":"Bill Murray starred in this time-altering comedy in the early 90s",
+                "format":"What is:",
+                "answer":"Groundhog's Day"   
+            },
+            {
+                "points":"200",
+                "question":"This film featured furry-actors as a recreation of Shakespeare's Hamlet ",
+                "format":"What is:",
+                "answer":"The Lion King"  
+            },
+            {
+                "points":"300",
+                "question":"This film was ground-breaking for CGI and the highest grossing feature of 1993",
+                "format":"What is:",
+                "answer":"Jurassic Park"  
+            }
+        ]
+    },
+    {
+        "category":"Science",
+        "questions":[
+            {
+                "points":"100",
+                "question":"This planet is actually closer to earth than our red-neighbor",
+                "format":"What is:",
+                "answer":"Venus"   
+            },
+            {
+                "points":"200",
+                "question":"This ocean creature and enemy of Ahab has the heaviest brain of any animal ",
+                "format":"What is a:",
+                "answer":"Sperm Whale"  
+            },
+            {
+                "points":"300",
+                "question":"This element is the most common in the human body",
+                "format":"What is",
+                "answer":"Oxygen"  
+            }
+        ]
+    }
+
+]
+export default gameQuestions;
